90f1a83c8d832c6b80a86e3e06e47fac9a4fb87b
tintheturtle <tinhoang@bu.edu>
2020-04-15 12:30:36 -0400

    successfully retrieved variants URL array in fetch request

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  event.respondWith(
+    handleRequest(event.request)
+  )
 })
+
+let URL = ''
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
+ 
 async function handleRequest(request) {
+  const response = await fetch('https://cfw-takehome.developers.workers.dev/api/variants')
+  const responseURLs = await response.json()
+  const variants = responseURLs.variants
+  console.log(variants)
+
+
   return new Response('Hello worker!', {
     headers: { 'content-type': 'text/plain' },
   })
