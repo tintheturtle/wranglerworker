961a878294c0ee8d48e5bf21b390b825b6422348
tintheturtle <tinhoang@bu.edu>
2020-04-15 22:40:07 -0400

    used HTMLRewriter API to change elements

--- index.js
+++ index.js
@@ -15,20 +15,43 @@ class AttributeRewriter {
   }
 
   element(element) {
-    const attribute = element.getAttribute(this.attributeName)
-    if (attribute) {
-          element.setAttribute(
-            this.attributeName,
-            attribute.replace('https://cloudflare.com', 'https://github.com/tintheturtle/wranglerworker')
-          )
-    }   
+
+    switch(this.attributeName) {
+      case 'title': {
+        element.setInnerContent(`Tin`)
+        break
+      }
+      case 'h1': {
+        element.setInnerContent(`Welcome! I'm Tin`)
+        break
+      }
+      case 'description': {        
+        element.setInnerContent(`This was actually a fun take home, I genuinely enjoyed doing it.`)
+        break
+      }
+      case 'href': {
+        const attribute = element.getAttribute(this.attributeName)
+        if (attribute) {
+              element.setAttribute(
+                this.attributeName,
+                attribute.replace('https://cloudflare.com', 'https://github.com/tintheturtle/wranglerworker')
+              )
+              element.setInnerContent('Check out my repo for this take home!')
+        } 
+        break
+      }
+    }
+      
   }
 }
 
 
 const rewriter = new HTMLRewriter()
+  .on('title', new AttributeRewriter('title'))
+  .on('h1#title', new AttributeRewriter('h1'))
+  .on('p#description', new AttributeRewriter('description'))
   .on('a#url', new AttributeRewriter('href'))
-  .on('a', new AttributeRewriter('text'))
+
  
 async function handleRequest(request) {
   // Getting the array of URLs from the api
